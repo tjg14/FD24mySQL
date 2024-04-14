0926bef7bb9f9da05f6c295e112c7b14fd65af8e
tjg14 <tgrigg2010@gmail.com>
2024-04-14 10:03:22 -0400

    changing sql connection

--- app.py
+++ app.py
@@ -28,21 +28,21 @@ Session(app)
 
 # Confirgure database connection locally
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:password123@localhost:3306/FD2024".format(
-    username="root",
-    password="password123",
-    hostname="localhost",
-    databasename="FD2024",
-)
-
-
-# Confirgure database connection for pythonanywhere
-# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://tjg14:TJGfd2024@tjg14.mysql.pythonanywhere-services.com:3306/tjg14$FD2024".format(
-#     username="tjg14",
-#     password="TJGfd2024",
-#     hostname="tjg14.mysql.pythonanywhere-services.com",
-#     databasename="tjg14$FD2024",
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:password123@localhost:3306/FD2024".format(
+#     username="root",
+#     password="password123",
+#     hostname="localhost",
+#     databasename="FD2024",
 # )
+
+
+#Confirgure database connection for pythonanywhere
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://tjg14:TJGfd2024@tjg14.mysql.pythonanywhere-services.com:3306/tjg14$FD2024".format(
+    username="tjg14",
+    password="TJGfd2024",
+    hostname="tjg14.mysql.pythonanywhere-services.com",
+    databasename="tjg14$FD2024",
+)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
